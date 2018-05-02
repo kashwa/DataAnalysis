a48667c184705dcabe7b749c95528153644af6fa
kashwa <kashwa80@gmail.com>
2018-05-02 13:24:45 +0200

    Test Req [3 : 6]

--- TestPython.py
+++ TestPython.py
@@ -3,17 +3,74 @@ import pandas as pd
 
 
 posts_sample = pd.read_csv("AnalyseData/Wuzzuf_Job_Posts_Sample.csv")
+app_sample = pd.read_csv("AnalyseData/Wuzzuf_Applications_Sample.csv")
 
 df = pd.DataFrame(posts_sample)
 
-# search for "category" in df.
-category1 = df.ix[:, 'job_category1']
+views = df.ix[:, 'views'].head(5)
+#siz = views.value_counts()
 
-'''The Count of each item => So, this can be slices'''
-siz = category1.value_counts()
+views_set = set(views)
+view_list = [x for x in views_set]
 
-'''To make it Unique => So, this can be [Labels]'''
-cat_set = set(category1)
+id = df.ix[:, 'id'].head(5)
+id_set = set(id)
+id_list = [i for i in id_set]
 
-plt.pie(siz, labels=cat_set, autopct='%1.1f%%')
+print(id_list)   # xLabel
+print(view_list) # yLabel
+
+
+plt.bar(id_list, view_list, label="bars")
+
+
+plt.legend()
 plt.show()
+
+'''citset = set(cities)
+
+
+citlist = [x for x in citset]
+
+
+print(citlist)
+
+print(siz)
+#citylist = set(cities)
+
+plt.bar(citlist, siz, label="address")
+
+#print(cities)
+
+plt.legend()
+plt.show()'''
+
+
+
+
+
+
+
+
+
+
+
+'''
+this is counting items in a list
+now i need to put user on x, count on y
+
+j=[]
+for x in usList:
+    j.append(usList.count(x))
+print(j)
+
+plt.bar(usList, j, label="bars")
+'''
+
+#countUser=''
+
+#plt.bar(usList, cntr)
+
+
+
+#print(countUser)
